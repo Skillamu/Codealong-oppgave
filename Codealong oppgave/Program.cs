b6cba5600184dc76e8b8a8fb8bea26a739d16194
ps4ca <ps4ca@DESKTOP-F0IB41B>
2023-03-30 19:00:40 +0200

    Add project files.

---
+++ Codealong oppgave/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace Codealong_oppgave
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var points = 0;
+
+            AskQuestion("Hva er 2+2?");
+            PossibleAnswers("2", "3", "4");
+            Console.WriteLine("Skriv inn a, b eller c: ");
+            points += CheckAnswer("c");
+
+            AskQuestion("Hvilken by er norsk?");
+            PossibleAnswers("Oslo", "Ohio", "Omaha");
+            Console.Write("Skriv inn a, b eller c: ");
+            points += CheckAnswer("a");
+
+            AskQuestion("Hvilken farge er ikke i den norske flagget?");
+            PossibleAnswers("Rød", "Grønn", "Blå");
+            Console.Write("Skriv inn a, b eller c: ");
+            points += CheckAnswer("b");
+
+            Console.WriteLine($"Du fikk {points} riktige."); 
+             
+
+            Console.ReadKey(true);
+        }
+
+        static void PossibleAnswers(params string[] alternatives)
+        {
+            var index = 0;
+            foreach (string alternative in alternatives)
+            {
+                var letter = Convert.ToChar('A' + index);
+                Console.WriteLine($"{letter} {alternative}");
+                index++;
+            }
+        }
+
+        static int CheckAnswer(string correctAnswer)
+        {
+            var answer = Console.ReadLine();
+            if (answer == correctAnswer)
+            {
+                Console.WriteLine("Riktig!");
+                return 1;
+            }
+
+            Console.WriteLine("Feil!");
+            return 0;
+        }
+
+        static void AskQuestion(string question)
+        {
+            Console.WriteLine(question);
+        }
+    }
+}
