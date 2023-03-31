898688c58a6d8632d6d232ad8333080ac56d66cf
Skillamu <ps4.c.aa@gmail.com>
2023-03-31 19:19:20 +0200

    Removed private field _points and made it a local variable in Run() instead because of immutability and it isn't used else where so no point of it being an instance variable i think.

--- Codealong oppgave/Quiz.cs
+++ Codealong oppgave/Quiz.cs
@@ -9,7 +9,6 @@ namespace Codealong_oppgave
     internal class Quiz
     {
         private Question[] _questions;
-        private int _points = 0;
         public Quiz()
         {
             _questions = new Question[]
@@ -22,13 +21,14 @@ namespace Codealong_oppgave
 
         public void Run()
         {
+            int points = 0;
             foreach (var question in _questions)
             {
-                           question.QuestionText();
-                           question.PossibleAlternatives();
-                _points += question.CheckAnswerFromUser();
+                          question.QuestionText();
+                          question.PossibleAlternatives();
+                points += question.CheckAnswerFromUser();
             }
-            Console.WriteLine($"Du fikk {_points} riktig.");
+            Console.WriteLine($"Du fikk {points} riktig.");
         }
     }
 }
